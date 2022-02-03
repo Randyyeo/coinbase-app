ba1c87adb93856bf89aaca3c580234b26127ade4
randall yeo <randall.yeo@isobar.com>
2022-02-03 17:24:07 +0800

    addition of dashboard pages

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,7 @@ import Dashboard from './Dashboard'
 export default function Home() {
   const {address, connectWallet} = useWeb3()
   return (
-    /* <Wrapper>
-      {address ? (
-        <Dashboard address={address} />
-      ) : (
-        <WalletConnect>
-          <Button onClick={() => connectWallet('injected')}>
-            Connect Wallet
-          </Button>
-          <Details>
-            You need Chrome to be 
-            <br /> able to run this app
-          </Details>
-        </WalletConnect>
-      )}
-    </Wrapper> */
+    
     <Wrapper>
       {address ? (
         <Dashboard address={address} />
